84f73748400867f74d2049ee76b414e8f0e8636d
Brett Lawson <brett19@gmail.com>
2018-05-15 15:46:49 -0700

    Update license to Couchbase Enterprise license.
    Change-Id: Ieaa3a982e2f2ab135b3e6f5ede6a3575f1297629
    Reviewed-on: http://review.couchbase.org/94259
    Reviewed-by: Charles Dixon <chvckd@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- aesprovider.go
+++ aesprovider.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018 Couchbase, Inc.
+ *
+ * Use of this software is subject to the Couchbase Inc. Enterprise Subscription License Agreement
+ * which may be found at https://www.couchbase.com/ESLA-11132015.
+ */
+
 package gocbfieldcrypt
 
 import (
@@ -20,6 +27,8 @@ type AesCryptoProvider struct {
 
 func (cp *AesCryptoProvider) getAlgNameFromKey(key, hmacKey []byte) (string, error) {
 	switch len(key) {
+	case 16:
+		return "AES-128-HMAC-SHA256", nil
 	case 32:
 		return "AES-256-HMAC-SHA256", nil
 	default:
